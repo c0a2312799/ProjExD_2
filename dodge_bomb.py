235699f537e977dbe861799c0c45a787f397b01d
hira <c0a2312799@edu.teu.ac.jp>
2024-10-01 14:12:33 +0900

    練習1:移動量辞書の定義

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,34 +4,43 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1100, 650
+DELTA = {pg.K_UP:(0, -5), 
+         pg.K_DOWN:(0, +5), 
+         pg.K_LEFT:(-5, 0), 
+         pg.K_RIGHT:(+5, 0),
+         }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
-    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    screen = pg.display.set_mode((WIDTH, HEIGHT))  # スクリーンの生成
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
-    kk_rct.center = 300, 200
+    kk_rct.center = 300, 200  # こうかとんの初期位置を設定
     clock = pg.time.Clock()
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-        screen.blit(bg_img, [0, 0]) 
+        screen.blit(bg_img, [0, 0])  # blitは背景画像貼り付け
 
-        key_lst = pg.key.get_pressed()
-        sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
+        key_lst = pg.key.get_pressed()  # キー反応するやつ
+        sum_mv = [0, 0]  # 横座標, 縦座標
+        #if key_lst[pg.K_UP]:
+        #    sum_mv[1] -= 5
+        #if key_lst[pg.K_DOWN]:
+        #    sum_mv[1] += 5
+        #if key_lst[pg.K_LEFT]:
+        #    sum_mv[0] -= 5
+        #if key_lst[pg.K_RIGHT]:
+        #    sum_mv[0] += 5
+        for key, tpl in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0] += tpl[0]  # 横方向
+                sum_mv[1] += tpl[1]  # 縦方向
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
